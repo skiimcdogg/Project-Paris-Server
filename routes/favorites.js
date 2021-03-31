240507b43f7c789f7deb06cb2b663af5b35d0168
Pauline Bellaud <pauline.bellaud@gmail.com>
2021-03-31 21:21:54 +0200

    favorites fixed

--- routes/favorites.js
+++ routes/favorites.js
@@ -4,28 +4,12 @@ const Favorites = require("./../models/Favorites");
 const User = require("./../models/User");
 const isLoggedIn = require("./../middlewares/isLoggedIn")
 
-router.get("/", isLoggedIn, (req, res, next) => {
-  Favorites.find()
-    .populate("favMuseums")
-    .populate("favMonuments")
-  .then((fav) => {
-    res.status(200).json(fav)
-    })
-    .catch(next);
-});
-
-// router.post("/add/:id", isLoggedIn, (req, res, next) => {
-//   const newFav = {
-//     favMuseums: req.params.id,
-//     favMonuments: req.params.id
-//   };
-//   Favorites.create(newFav)
-//     .then((createdFav) => {
-//         const favId = createdFav._id
-//         User.findByIdAndUpdate({_id: req.session.currentUser._id}, {$push: {favorites: favId}}, {new: true})
-//         .then((favRes) => {
-//             res.status(200).json(favRes);
-//         })
+// router.get("/", isLoggedIn, (req, res, next) => {
+//   Favorites.find()
+//     .populate("favMuseums")
+//     .populate("favMonuments")
+//   .then((fav) => {
+//     res.status(200).json(fav)
 //     })
 //     .catch(next);
 // });
@@ -39,7 +23,12 @@ router.post("/add/:id", isLoggedIn, async (req, res, next) => {
   let createdFav = await Favorites.create(newFav)
   const favId = createdFav._id
   let updatedUser = await User.findByIdAndUpdate({_id: req.session.currentUser._id}, {$push: {favorites: favId}}, {new: true}).select("-password")
-  updatedUser = await updatedUser.populate('favorites').execPopulate()
+  updatedUser = await updatedUser.populate({
+    path: "favorites",
+    populate: {
+      path: "favMuseums favMonuments"
+    }
+  }).execPopulate()
    res.status(200).json(updatedUser);
     }
     catch (err) {
@@ -47,11 +36,10 @@ router.post("/add/:id", isLoggedIn, async (req, res, next) => {
     }
 });
 
-router.delete("/delete/:id", isLoggedIn, (req, res, next) => {
+router.delete("/delete/:id", (req, res, next) => {
   Favorites.findByIdAndDelete(req.params.id)
     .then((response) => {
-        // User.findByIdAndUpdate({_id: req.session.currentUser._id}, {$pull: {favorites: {id: req.params.id}}})
-        res.status(200).json(response)
+      res.status(200).json(response)
     })
     .catch(next);
 });
